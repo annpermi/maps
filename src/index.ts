c80ab54f346b12e18a84a290c6119b9226591b9e
annpermi <perminova.anna85@gmail.com>
2022-01-26 22:30:12 -0500

    Installed @types/google.maps

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log('hi there');
+import { User } from "./User";
+import { Company } from "./Company";
+
+const user = new User();
+/* 
+location: {lat: 3.5458, lng: -66.3042}
+name: "Raphaelle"
+*/
+
+const company = new Company();
+/* 
+catchPhrase: "Optional homogeneous application"
+companyName: "Davis, Schuppe and Reinger"
+location: {lat: 83.4425, lng: -75.128}
+*/
+
+console.log({ user });
+console.log({ company });
+
+//npm install @types/google.maps
+google.maps;
