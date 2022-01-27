d55578298502bc7d7804dfe18e851f304ecb52c3
annpermi <perminova.anna85@gmail.com>
2022-01-26 22:42:00 -0500

    Added google map on the UI

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,29 @@
-import { User } from "./User";
-import { Company } from "./Company";
+// import { User } from "./User";
+// import { Company } from "./Company";
 
-const user = new User();
-/* 
-location: {lat: 3.5458, lng: -66.3042}
-name: "Raphaelle"
-*/
+// const user = new User();
+// /*
+// location: {lat: 3.5458, lng: -66.3042}
+// name: "Raphaelle"
+// */
 
-const company = new Company();
-/* 
-catchPhrase: "Optional homogeneous application"
-companyName: "Davis, Schuppe and Reinger"
-location: {lat: 83.4425, lng: -75.128}
-*/
+// const company = new Company();
+// /*
+// catchPhrase: "Optional homogeneous application"
+// companyName: "Davis, Schuppe and Reinger"
+// location: {lat: 83.4425, lng: -75.128}
+// */
 
-console.log({ user });
-console.log({ company });
+// console.log({ user });
+// console.log({ company });
 
 //npm install @types/google.maps
-google.maps;
+//created instance of Map class
+
+new google.maps.Map(document.getElementById("map"), {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
