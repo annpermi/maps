1f51d48169f76e210afa29d45460e5225f778b3e
annpermi <perminova.anna85@gmail.com>
2022-01-26 21:33:39 -0500

    1. Installed packages, 2. Created User class

---
+++ src/User.ts
@@ -0,0 +1,23 @@
+class User {
+  name: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
+
+//install
+// npm install faker@5.5.3
+// npm install @types/faker@5.5.9
+
+/* 
+https://github.com/faker-js/faker
+
+If you wish to use this library instead, you can install it by running:
+
+npm install @faker-js/faker
+
+You'll then need to update your imports:
+
+import faker from '@faker-js/faker';
+*/
